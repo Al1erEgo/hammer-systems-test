0f0714a6f7bca167dc71a6f09c0a2b8de96b334b
Al1erEgo <al1erego@gmail.com>
2023-06-17 20:28:10 +0200

    feat: try to render table st2

--- src/views/app-views/main/constructor/Element.js
+++ src/views/app-views/main/constructor/Element.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Image } from 'antd';
 
 const Element = () => {
     return (
-        <Image
-            width={100}
-            src="/img/elements/table.png"
-        />
+        <img src="./hammer-systems-test/img/elements/table.png" alt='table'/>
     );
 };
 
